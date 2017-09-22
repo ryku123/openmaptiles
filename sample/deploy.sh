06c029d672539e2ba5f015be6cd60438ee9a1966
jirik <jirik@users.noreply.github.com>
2017-09-22 12:11:36 +0200

    Generate sample of railway branch

---
+++ sample/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# https://docs.travis-ci.com/user/customizing-the-build#Implementing-Complex-Build-Steps
+set -ev
+
+git config --global user.email "openmaptiles@klokantech.com"
+git config --global user.name "OpenMapTiles Travis"
+
+# deploy
+cp ./quickstart.log ./data/
+cd data
+git init
+git add tiles.mbtiles
+git add quickstart.log
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
